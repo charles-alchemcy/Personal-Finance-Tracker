34635e83b3410064097d365216a2838374f13f9e
charles-alchemcy <CharlesDevAlchemy@gmail.com>
2025-11-07 09:06:44 -0500

    add database user dashiboard

--- models/Expense.js
+++ models/Expense.js
@@ -15,5 +15,17 @@ const cardSchema = new mongoose.Schema({
     description: { type: String }
 });
 
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    birthday: { type: Date, default: Date.now },
+    role: {type : String , enum : ["user" , "admin"]},
+});
+
+const dashboardSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    create : { type: Date, default: Date.now },
+    count : {type : Number , required: true},
+});
+
 
 module.exports = mongoose.model('Expense', expenseSchema);
